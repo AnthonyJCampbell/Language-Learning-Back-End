c9d0d17fe6600a6a488f9d4f0013da0ba0d721ee
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-13 12:13:37 +0200

    Migration for sessions && initialized seed for test_users

---
+++ data/migrations/20190413115232_CreateSessionTable.js
@@ -0,0 +1,28 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('sessions', sessions => {
+    sessions
+      .increments('session_id')
+    sessions
+      .integer('user_id', 126)
+      .notNullable()
+      .unsigned()
+      .references('user_id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+      sessions
+        .timestamp('start_of_session')
+        .notNullable();
+      sessions
+        .integer('length_of_session')
+        .nullable();
+      sessions
+        .timestamp('end_of_session')
+        .nullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('sessions');
+};
