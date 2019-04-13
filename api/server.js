9be721f81de6f60540dac44d1a511d0b272a4842
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-13 13:47:48 +0200

    Added dotenv and fixed server. Verified server is working

--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.status(200).json({ "message": "Server's alive!"})
+})
+
 // Routing && prefixing URLs
 // server.use('/api/dishes', routeName)
 
