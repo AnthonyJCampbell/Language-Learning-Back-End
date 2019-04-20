a4c9bc78e832bbd0a7a41cbcde55a10e9419f423
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-20 12:27:18 +0200

    Spell-check

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,4 +2,4 @@ const knex = require('knex');
 const config = require('../knexfile');
 const dbEnv = process.env.DB_ENV || 'development';
 
-module.exports = knex(config[dbEnv]);;
+module.exports = knex(config[dbEnv]);
