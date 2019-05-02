239498d1c13b6785cfc36995beb58f2f7ad77635
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-02 15:58:25 +0200

    Bug fix in token-generator and implementing token on registration route

--- data/routes/loginAndRegistrationRoutes.js
+++ data/routes/loginAndRegistrationRoutes.js
@@ -56,44 +56,49 @@ router.post('/login', (req, res) => {
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = tokenService(user);
           // RETURNS A MESSAGE, A TOKEN, AND THE USER OBJECT
-          res.status(200).json({
+          return res.status(200).json({
             token,
             user
           });
         }
         // FAIL: INCORRECT PASSWORD
         if (user && !bcrypt.compareSync(password, user.password)) {
-          res.status(404).json({ message: 'Invalid password!' });
+          return res.status(404).json({ message: 'Invalid password!' });
         }
         // FAIL: INCORRECT USERNAME (DEFAULT)
         else {
-          res.status(404).json({
+          return res.status(404).json({
             message: `There's no user with an 'username' of ${req.body.username}`
           });
         }
       })
       .catch(() => {
-        res.status(500).json({ message: "Something's gone wrong!"})
+        return res.status(500).json({ message: "Something's gone wrong!"})
       })
   }
 })
 
 // REGISTER
 // Both username, email_address are required to be unique in the DB.
+// Returns a token and a user object
 router.post('/register', (req, res) => {
   const { email_address, password } = req.body;
   const username = `user-${email_address}`
   
   if (!username || !email_address || !password) {return res.status(404).json({message: "Make sure to pass a 'username', 'email_address', and 'password"})}
   if (!email_address.includes('@') || !email_address.includes('.')) {return res.status(404).json({message: "Make sure to pass a valid email address!"})}
-  // Check email address for special characters that we don't use in the DB
+  // >>> Check email address for special characters that we don't use in the DB
   
   users.addUser({"email_address": email_address, "password": password, "username": username})
-    .then(data => {
-      res.status(200).json(data)
+    .then(user => {
+      const token = tokenService(user);
+      return res.status(200).json({
+        token,
+        user
+      })
     })
     .catch(() => {
-      res.status(500).json({message: "Whoops!"})
+      return res.status(500).json({message: "Whoops!"})
     })
 })
 
