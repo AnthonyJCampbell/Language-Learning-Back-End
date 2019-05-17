189c3ca18b35885d3b0b1be099775bd5b7c3559c
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-17 10:59:21 +0200

    Production knex

--- knexfile.js
+++ knexfile.js
@@ -15,4 +15,17 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/db.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 };
