1b964b1e09e6954fbabf107b69cb9cf5bb6e963b
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-18 19:56:54 +0200

    Altering process.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 6666;
-server.listen(port, () => console.log(`
-  ** Running on port ${port}**
-`));
+const port = process.env.PORT || 7000;
+server.listen(port, () => console.log(
+  `** Running on port ${port} **`
+));
