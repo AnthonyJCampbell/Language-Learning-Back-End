f283777a9e2213d5c73f590eeefe64a993cb40ee
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-18 21:26:06 +0200

    Fixing Server message

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 7000;
 server.listen(port, () => console.log(
-  `** Running on port ${port} **`
+  `\n** Running on port ${port} **\n`
 ));
