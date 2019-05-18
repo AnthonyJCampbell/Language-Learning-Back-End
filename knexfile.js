7346619651e56184131b1b09d5011f007d01c9a4
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-05-18 19:52:07 +0200

    Resetting knexfile.js & adding PostGreSQL

--- knexfile.js
+++ knexfile.js
@@ -5,27 +5,45 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db.sqlite3'
+      filename: './data/db.db3'
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: './data/migrations'
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: './data/seeds'
     }
   },
-  production: {
+
+  testing: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db.sqlite3'
+      filename: './data/test.db3'
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: './data/seeds',
     }
   },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
 };
