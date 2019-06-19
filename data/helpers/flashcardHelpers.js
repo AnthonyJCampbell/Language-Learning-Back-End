b5dfc0a1a48a1f4f2cf111d67a9e6a2421fa19a6
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-06-19 14:11:11 +0200

    Added and scaffolded flashcardHelpers.js

---
+++ data/helpers/flashcardHelpers.js
@@ -0,0 +1,39 @@
+const mongodb = require('mongodb'); 
+
+const getFlashcards = () =>  {
+
+}
+
+const getFlashcardById = (id) => {
+
+}
+
+const getXNumberOfFlashcards = (numberOfFlashcards) => {
+
+}
+
+const insertOneFlashcard = (flashcard) => {
+
+}
+
+const insertManyFlashcards = (flashcards) => {
+
+}
+
+const editFlashcard = (id) => {
+
+}
+
+const deleteFlashcard = (id) => {
+
+}
+
+module.exports = {
+  getFlashcards,
+  getFlashcardById,
+  getXNumberOfFlashcards,
+  insertOneFlashcard,
+  insertManyFlashcards,
+  editFlashcard,
+  deleteFlashcard
+}
