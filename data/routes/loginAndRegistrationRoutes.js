9af43c3a3a2242424ce286858690a790de9216a7
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-06-20 11:51:32 +0200

    Scaffolding for Login. Now implementing Mongo

--- data/routes/loginAndRegistrationRoutes.js
+++ data/routes/loginAndRegistrationRoutes.js
@@ -12,49 +12,56 @@ const users = require('../helpers/userHelpers');
 // LOGIN
 router.post('/login', (req, res) => {
   const password = req.body.password
-  const email = req.body.identifier;
+  const email = req.body.email;
   
-  if (!identifier|| !password) {
+  if (!email|| !password) {
     return res.status(404).json({
-      message: "Please provide an identifier (either username or password) and a 'password'"
+      message: "Please provide an email address and a password"
     })
   }
   
-  // Login with email is the default, since new sign-ups still have automatically-generated usernames
-  if (email_address.includes('@') && email_address.includes('.') && email.length >= 10) {
-    users.getUserByEmail(email_address)
-      .then(user => {
-      // SUCCESS CASE: CORRECT USERNAME & PASSWORD.
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = tokenService(user);
-          // RETURNS A MESSAGE, A TOKEN, AND THE USER OBJECT
-          return res.status(200).json({
-            token,
-            user
-          });
-        }
-        // FAIL: INCORRECT PASSWORD
-        if (user && !bcrypt.compareSync(password, user.password)) {
-          return res.status(404).json({ 
-            message: 'Invalid password!' 
-          });
-        }
-        // FAIL: INCORRECT USERNAME (DEFAULT)
-        else {
-          return res.status(404).json({
-            message: `There's no user with an 'email_address' of ${req.body.email_address}`
-          });
-        }
-      })
-      .catch(() => {
-        return res.status(500).json({ 
-          message: "Something's gone wrong!"
-        })
-      })
-
-  } else {
-    
+  if (!email_address.includes('@') || !email_address.includes('.') || email.length < 10) {
+    return res.status(400).json({
+      message: "Make sure to pass a valid email address!"
+    })
   }
+
+  // Disabled during development
+  // if (password.length < 8 || password == password.toLowerCase()) {
+  //   return res.status(400).json({
+  //     message: "Invalid password"
+  //   })
+  // }
+
+  users.getUserByEmail(email_address)
+    .then(user => {
+    // SUCCESS CASE: CORRECT USERNAME & PASSWORD.
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService(user);
+        // RETURNS A MESSAGE, A TOKEN, AND THE USER OBJECT
+        return res.status(200).json({
+          token,
+          user
+        });
+      }
+      // FAIL: INCORRECT PASSWORD
+      if (user && !bcrypt.compareSync(password, user.password)) {
+        return res.status(404).json({ 
+          message: 'Invalid password!' 
+        });
+      }
+      // FAIL: INCORRECT USERNAME (DEFAULT)
+      else {
+        return res.status(404).json({
+          message: `There's no user with an 'email_address' of ${req.body.email_address}`
+        });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ 
+        message: "Something's gone wrong!"
+      })
+    })
 })
 
 // REGISTER
@@ -74,7 +81,6 @@ router.post('/register', (req, res) => {
   }
   // Disabled during development
   // if (password.length < 8 || password == password.toLowerCase()) {
-  //   console.log(password.length)
   //   return res.status(400).json({
   //     message: "Make sure your password is at least 8 characters long and contains at least one uppercase letter!"
   //   })
