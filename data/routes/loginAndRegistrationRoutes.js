cfe1c11a90f38438d7a1f25d40867f28e52314e5
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-06-20 11:47:14 +0200

    Removing login w/ username

--- data/routes/loginAndRegistrationRoutes.js
+++ data/routes/loginAndRegistrationRoutes.js
@@ -12,15 +12,16 @@ const users = require('../helpers/userHelpers');
 // LOGIN
 router.post('/login', (req, res) => {
   const password = req.body.password
-  const email_address = req.body.email_address;
-  // const username = req.body.email_address;
+  const email = req.body.identifier;
   
-  if ((!req.body.username && !req.body.email_address) || !req.body.password) {
-    return res.status(404).json({message: "Please provide either a 'username' or a 'email_address' and a 'password'"})
+  if (!identifier|| !password) {
+    return res.status(404).json({
+      message: "Please provide an identifier (either username or password) and a 'password'"
+    })
   }
   
   // Login with email is the default, since new sign-ups still have automatically-generated usernames
-  if (email_address.includes('@') && email_address.includes('.')) {
+  if (email_address.includes('@') && email_address.includes('.') && email.length >= 10) {
     users.getUserByEmail(email_address)
       .then(user => {
       // SUCCESS CASE: CORRECT USERNAME & PASSWORD.
@@ -52,42 +53,11 @@ router.post('/login', (req, res) => {
       })
 
   } else {
-    // Login with Username
-    users.getUser(username)
-      .then(user => {
-        // SUCCESS CASE: CORRECT USERNAME & PASSWORD.
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = tokenService(user);
-          // RETURNS A MESSAGE, A TOKEN, AND THE USER OBJECT
-          return res.status(200).json({
-            token,
-            user
-          });
-        }
-        // FAIL: INCORRECT PASSWORD
-        if (user && !bcrypt.compareSync(password, user.password)) {
-          return res.status(404).json({ 
-            message: 'Invalid password!' 
-          });
-        }
-        // FAIL: INCORRECT USERNAME (DEFAULT)
-        else {
-          return res.status(404).json({
-            message: `There's no user with an 'username' of ${req.body.username}`
-          });
-        }
-      })
-      .catch(() => {
-        return res.status(500).json({ 
-          message: "Something's gone wrong!"
-        })
-      })
+    
   }
 })
 
 // REGISTER
-// Both username, email_address are required to be unique in the DB.
-// Returns a token and a user object
 router.post('/register', (req, res) => {
   let { name, email, password } = req.body;
   
